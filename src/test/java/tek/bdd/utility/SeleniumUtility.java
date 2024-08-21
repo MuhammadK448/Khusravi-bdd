29bb5523fc62c7f04337f708bcfcc4051fd4f507
MJK <muhammadkhusravi@gmail.com>
2024-08-20 20:06:18 -0700

    Selenium Utilities class made maintainable

--- src/test/java/tek/bdd/utility/SeleniumUtility.java
+++ src/test/java/tek/bdd/utility/SeleniumUtility.java
@@ -1,6 +1,7 @@
 package tek.bdd.utility;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import tek.bdd.base.BaseSetup;
@@ -13,6 +14,9 @@ public class SeleniumUtility extends BaseSetup {
         return new WebDriverWait(getDriver(), Duration.ofSeconds(20));
     }
 
+    private WebElement waitForVisibility(By locator){
+        return getWait().until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
     //Create a method to click on a given locator
     public void clickOnElement(By locator) {
         getWait().until(ExpectedConditions.elementToBeClickable(locator)).click();
@@ -20,6 +24,11 @@ public class SeleniumUtility extends BaseSetup {
 
     //Enter text to the located element
     public void sendText(By locator, String value) {
-        getWait().until(ExpectedConditions.visibilityOfElementLocated(locator)).sendKeys(value);
+        waitForVisibility(locator).sendKeys(value);
+    }
+
+    //Create method for getting the text of a located element
+    public String getText(By locator){
+       return waitForVisibility(locator).getText();
     }
 }
